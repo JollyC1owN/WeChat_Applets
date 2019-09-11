18ca458e0ebd06f7beb6dc4665165c8e8ea2a43d
tomb <tomb@qq.com>
2019-09-11 20:22:04 +0800

    change1

---
+++ pages/index/index.js
@@ -0,0 +1,91 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo:{}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 获取用户的登录信息
+    wx.getUserInfo({
+      success:(data)=>{
+        this.setData({
+          userInfo: data.userInfo
+        })
+      },
+      fail:()=>{
+        console.log("获取用户信息失败")
+      }
+    })
+  },
+  goList(){
+    // 去list页面
+    wx.redirectTo({
+      url:'/pages/list/list'
+    })
+  },
+  handelGetUserInfo(info){
+    // 判断用户点击的是否是允许
+    if(info.detail.rawData){
+      this.setData({
+        userInfo: JSON.parse(info.detail.rawData)
+      })
+    }
+  },
+  
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/list/list.js
@@ -0,0 +1,78 @@
+// pages/list/list.js
+let datas = require('../../datas/list-data.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    listArr:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 更新状态数据
+    this.setData({
+      listArr: datas.list_data
+    })
+  },
+  goDetail(event) {
+    // 准备数据接收下标
+    let index = event.currentTarget.dataset.index
+    // 跳转到详情页
+    wx.navigateTo({
+      // query传参
+      url: '/pages/detail/detail?index='+index,
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
